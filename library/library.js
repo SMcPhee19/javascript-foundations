5cbc364330b94030d052e9235f41e42580fd485e
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-12-04 17:26:56 -0700

    Finish Library

--- library/library.js
+++ library/library.js
@@ -1,3 +1,36 @@
+function issueCard(name, age, numBooksCheckedOut = 0, isChild) {
+  if (age < 18) {
+    isChild = true
+  } else {
+    isChild = false
+  }
+
+  return {
+    name,
+    age,
+    numBooksCheckedOut,
+    isChild
+  }
+}
+
+function searchByAuthor(collectionArray, name) {
+  returnedCollection = [];
+
+  collectionArray.forEach(book => {
+    if (book.author == name) {
+      returnedCollection.push(book);
+    }
+  });
+
+  if (returnedCollection.length === 0) {
+    return 'No book found for search criteria';
+  } else {
+    return returnedCollection;
+  }
+}
 
 
-module.exports = { };
+module.exports = { 
+  issueCard,
+  searchByAuthor
+};
