e7f2d6ec4eb084d4d2daf49c436be09194038aa9
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-12-04 18:11:55 -0700

    finish VHS

--- vhs/vhs.js
+++ vhs/vhs.js
@@ -5,6 +5,35 @@ function createTape(title, readyToPlay = false) {
   }
 }
 
+function reset(movie) {
+  if (movie.readyToPlay == false) {
+    movie.readyToPlay = true
+    return movie
+  } else {
+    return movie
+  }
+}
+
+function createCollection(...movies) {
+  if (movies.length === 0) {
+    return 'Your collection is empty.'
+  } else {
+    collection = movies
+    return collection
+  }
+}
+
+function previewCollection(collection) {
+  collectionTitles = []
+  collection.forEach(movie => {
+    collectionTitles.push(movie.title)
+  });
+  return collectionTitles
+}
+
 module.exports = { 
-  createTape
+  createTape,
+  reset,
+  createCollection,
+  previewCollection
 }
