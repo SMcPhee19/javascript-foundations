1df8ccd13511f5de1ef7705a7dc7043d5fbdde8b
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-12-07 14:59:02 -0700

    complete barber

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,47 @@
+function createBarber(name, earnings = 0, cutsArray) {
+  haircutsArray = []
+  if (cutsArray == undefined) {
+    haircutsArray = []
+  } else {
+    cutsArray.forEach(cut => {
+      haircutsArray.push(cut)
+    });
+  }
 
+  return {
+    name,
+    earnings,
+    haircuts: haircutsArray
+  }
+}
 
-module.exports = {  };
+function giveCompliment(haircut) {
+  return `This ${haircut.style} looks great!`
+}
+
+function cutHair (barber, cut) {
+  barberCuts = barber.haircuts
+  barberCuts.push(cut)
+
+  barber.earnings += cut.price
+  return barber
+}
+
+function listStyles (barber, styleLength) {
+  const barberCuts = barber.haircuts.slice();
+
+  barberKnownCutNames = []
+  barberCuts.forEach(cut => {
+    if (cut.hairLength == styleLength) {
+      barberKnownCutNames.push(cut.style)
+    }
+  });
+  return barberKnownCutNames
+}
+
+module.exports = {
+  createBarber,
+  giveCompliment,
+  cutHair,
+  listStyles
+};
