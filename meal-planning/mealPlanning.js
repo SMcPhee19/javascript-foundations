2cefd60dc6af4e4cfbf0b5fd23c38c1a05afedda
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-12-07 16:24:19 -0700

    finish meal prep

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,30 @@
+function createMeal(type, calorieGoal, dishes = []) {
+  return {
+    type,
+    calorieGoal,
+    dishes
+  }
+}
 
-module.exports = { }
+function addDish(meal, dish) {
+  if (meal.calorieGoal >= dish.calories) {
+    meal.calorieGoal -= dish.calories
+    meal.dishes.push(dish)
+  }
+  return meal
+}
+
+function calculateCalories(meal) {
+  totalCalories = 0
+  meal.dishes.forEach(dish => {
+    totalCalories += dish.calories
+  });
+ 
+  return `${meal.type} has a total of ${totalCalories} calories.`
+}
+
+module.exports = {
+  createMeal,
+  addDish,
+  calculateCalories
+}
